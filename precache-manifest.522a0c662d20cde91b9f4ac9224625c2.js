4e009240a5c5a79f39d4558e8e983e05eb690ea2
Kylie Stanley <knstanley11@gmail.com>
2019-01-06 21:26:55 -0700

    Updates

---
+++ precache-manifest.522a0c662d20cde91b9f4ac9224625c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/swapi-box/static/media/Starjhol.9c91297e.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/swapi-box/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "37aee6c8c1d16c287d66",
+    "url": "/swapi-box/static/js/runtime~main.37aee6c8.js"
+  },
+  {
+    "revision": "0bc068fe96c35cbae2a4",
+    "url": "/swapi-box/static/js/main.0bc068fe.chunk.js"
+  },
+  {
+    "revision": "5efdcfbaaa05de7ea329",
+    "url": "/swapi-box/static/js/1.5efdcfba.chunk.js"
+  },
+  {
+    "revision": "0bc068fe96c35cbae2a4",
+    "url": "/swapi-box/static/css/main.fc30fb40.chunk.css"
+  },
+  {
+    "revision": "263edb5d80fbfc450604e9187b2e3fa6",
+    "url": "/swapi-box/index.html"
+  }
+];
